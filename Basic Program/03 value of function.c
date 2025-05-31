55bfa4eceef2ae7e8b1dea3ff3c3d606ccef523a
GuruJadhav2007 <guruprasadjadhav911@gmail.com>
2025-05-31 07:41:29 +0530

    Define value of functions

---
+++ Basic Program/03 value of function.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main ()
+{
+
+    char ch = 'z';
+    int Num = 21;
+    float per = 89.4875;
+
+    printf ("In Welcome To programming");
+    printf("\n value of Num =%d $$", Num);
+
+    printf("\n value of character => %c !!", ch);
+
+    printf("\n percentage = %f in STI", per);
+
+    getch();
+    return 0;
+}
