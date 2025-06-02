8793a546738ac2366c5c8e3e9b2341e305de6bb5
GuruJadhav2007 <guruprasadjadhav911@gmail.com>
2025-06-02 07:45:05 +0530

    09 use if .c

---
+++ Basic Program/09 use of if in program.c
@@ -0,0 +1,26 @@
+/* Author : Fork infosystem {multi line comment}
+   Date.  : 16-5-2025
+   For.   : Vecation Batch
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+
+{
+    int Num =120;
+
+    printf("\n Hello");//single line comment 
+
+    if (Num > 100)
+
+    {
+       printf("\n\t Welcome");
+    }
+
+    printf("\n Good Bye");
+
+    getch();
+    return 0;
+}
