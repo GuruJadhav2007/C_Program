8a35592c87ec31f6470d5d765b5c8ba14c7949dd
GuruJadhav2007 <guruprasadjadhav911@gmail.com>
2025-06-03 06:41:31 +0530

    Use if with user define value

---
+++ Basic Program/10 use if with user defined value.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+
+{
+  int No = 0;
+  
+  printf("\n\t Hello");
+  
+  printf("\n\n Enter Number Greater Than 100.");
+  scanf("%d",&No);
+  
+  if( No > 100)
+  
+ {
+   printf("\n\n Thank You For Entering Greater Number");
+ }
+  
+  printf("\n\n Good Bye");
+  
+  getch();
+  return 0;
+  
+  
+}
