30a7af88b6bd90bbcdf74c976e8df9c47f7fea90
GuruJadhav2007 <guruprasadjadhav911@gmail.com>
2025-06-03 06:42:14 +0530

    Use if else

---
+++ Basic Program/11 use of if else in program.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+{
+  int No =0;
+  
+  printf("\n\t\t (°-°) \t Hello \t (°-°)");
+  
+                  
+  printf("\n Enter positive no. :");
+  scanf("%d",&No);
+  
+  if(No > 0)
+  
+  { 
+    printf("\n Welcome You Enter positive number");
+  }
+  else
+  { 
+    printf("\n Sorry You Enter negative number");
+  }
+   printf("\n Thank You For Entering Number");
+   
+   getch();
+   return 0;
+ 
+  }
+ 
+  
+  
+  
