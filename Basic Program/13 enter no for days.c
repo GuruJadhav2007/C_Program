a714001c093d4f92c28d772d4370f677f216dc93
GuruJadhav2007 <guruprasadjadhav911@gmail.com>
2025-06-04 08:21:30 +0530

    Enter number for days

---
+++ Basic Program/13 enter no for days.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+
+{
+  int Num = 0;
+  
+  printf("\n Enter Number 1-7 : ");
+  scanf("%d",&Num);
+  
+  if( Num ==1)h7< 
+   {
+     printf("\n\n Monday");
+   }
+  
+  else if( Num == 2)
+   {
+     printf("\n\n Tuesday ");
+   }
+  
+  else if ( Num == 3)
+   {
+     printf("\n\n Wednesday ");
+   }
+  
+  else if( Num == 4)
+   {
+     printf("\n\n Thursday ");
+   }
+  else if ( Num == 5)
+   {
+     printf("\n\n Friday ");
+   }
+  
+  else if( Num == 6)
+   {
+     printf("\n\n Saturday ");
+   }
+  
+  else if( Num == 7)
+   {
+     printf("\n\n Sunday ");
+   }
+  
+  else 
+  {
+    printf("\n\n Sorry! Invalid Input  ");
+    
+  }
+  
+  getch();
+  return 0;
+}
