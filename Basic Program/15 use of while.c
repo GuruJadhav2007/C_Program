c42d170e2cb4d934dac972e466a518650dbe7dd7
GuruJadhav2007 <guruprasadjadhav911@gmail.com>
2025-06-06 06:53:54 +0530

    Use while

---
+++ Basic Program/15 use of while.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main ()
+
+{
+  int A = 1;
+  
+  printf("\n\n Welcome ");
+  
+  while(A <=100 )
+  {
+    printf("\n\n %d * Fork Infosystem *",A); 
+     
+    A++;
+  }
+  
+  printf("\n\n Thank You ");
+  
+  getch();
+  return 0;
+}
