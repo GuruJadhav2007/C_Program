c4c09a3b05c6d76ae1ff99d72528415a487a51e8
GuruJadhav2007 <guruprasadjadhav911@gmail.com>
2025-06-06 06:53:04 +0530

    Add files via upload

---
+++ Basic Program/14 use of for.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+
+{
+  int i = 0 ;
+  
+  printf("\n\n Welcome ");
+  
+  for ( i =1 ; i<=10 ; i++)
+  {
+    printf("\n\n %d 🙏 Jay Shree Ram. 🙏" ,i);
+  }
+  printf("\n\n Thank You ");
+  
+  getch();
+  return 0;
+} 
