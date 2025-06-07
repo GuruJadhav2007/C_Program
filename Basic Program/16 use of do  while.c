281e08a3abfb41959d8fe5eacdd04b8c899ec225
GuruJadhav2007 <guruprasadjadhav911@gmail.com>
2025-06-07 16:42:16 +0530

    Use do while

---
+++ Basic Program/16 use of do  while.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main ()
+{
+  int j = 1;
+  
+  printf("\n\n Welcome ");
+  
+  do
+  {
+    printf("\n\n %d India",j);
+    j++;
+  }
+  
+  while(j<=10);
+ 
+  
+  printf("\n\n Thank You ");
+  
+  getch();
+  return 0;
+}
