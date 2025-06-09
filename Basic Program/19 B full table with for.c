612caafe4f310a0c26acec4696e627ffb00d0206
GuruJadhav2007 <guruprasadjadhav911@gmail.com>
2025-06-09 12:34:52 +0530

    Full table (for)

---
+++ Basic Program/19 B full table with for.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+{
+  int No = 0, Multi = 1;
+  
+  for( No =20; Multi <= 10; Multi++)
+  {
+    printf("\n %d * %d = %d ",No,Multi,(No * Multi));
+   
+  }
+  
+  printf("\n\n Thank You");
+  
+  getch();
+  return 0;
+
+}
