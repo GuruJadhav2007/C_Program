f2d3918ba811c5860d15820b653760a0f7e3ceb3
Guruprasad_Jadhav <guruprasadjadhav911@gmail.com>
2025-07-24 15:08:41 +0530

    If_Else

---
+++ Control_Statments/03 If_Else.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+
+{
+    int Num = 20;
+
+    printf("\n\n Hello ");
+
+    if( Num > 0)
+    {
+        printf("\n\n Welcome");
+    }
+    else
+     {
+        printf("\n\n Sorry! Enter Number Greater Than 0");
+     }
+
+     printf("\n\n Thank You");
+
+     getch();
+     return 0;
+}
