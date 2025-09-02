1605a6f0e33775fae6b6479666829369125aaeca
Guruprasad_Jadhav <guruprasadjadhav911@gmail.com>
2025-09-02 14:47:41 +0530

    B Student Information Array in Structure Case Study

---
+++ Case_Study_of_C/02 B_Student_Information_Array_in_Structure_Case_Study.c
@@ -0,0 +1,76 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+struct Stud
+{
+    int Roll_No;
+    char Stud_Name[25];
+    int M_Phy;
+    int M_Chem;
+    int M_Maths;
+    int Total;
+    int Percentage;
+
+};
+
+int main()
+{
+    int i = 0, Std_Cnt = 0;
+    struct Stud* Std = NULL;
+
+    printf("\n Enter Student of Count : ");
+    scanf("%d",&Std_Cnt);
+
+    Std = (struct stud*) malloc(Std_Cnt * sizeof(struct Stud));
+
+    printf("\n\n Enter Student Details => \n\n");
+
+    for(i = 1; i <= Std_Cnt; i++)
+    {
+        Std[i].Roll_No = i + 100;
+        printf("\n Enter Student Details For Roll No %d => \n", i+100);
+
+        fflush(stdin);
+        printf("\n Enter Student Name : ");
+        scanf("%s",&Std[i].Stud_Name);
+        fflush(stdin);
+
+        printf("\n Enter Physics Marks : ");
+        scanf("%d",&Std[i].M_Phy);
+
+        printf("\n Enter Chemistry Marks : ");
+        scanf("%d",&Std[i].M_Chem);
+
+        printf("\n Enter Mathematics Marks : ");
+        scanf("%d",&Std[i].M_Maths);
+
+        Std[i].Total = Std[i].M_Phy + Std[i].M_Chem + Std[i].M_Maths;
+
+        Std[i].Percentage = Std[i].Total / 3;
+
+        system("cls");
+
+    }
+
+    printf("\n\n Student Details are => \n");
+
+    for(i = 1; i <= Std_Cnt; i++)
+    {
+        printf("\n\n===================================================");
+
+        printf("\n Student Roll No is           : %d", Std[i].Roll_No);
+        printf("\n Student Name is              : %s", Std[i].Stud_Name);
+        printf("\n Student Physics Marks        : %d", Std[i].M_Phy);
+        printf("\n Student Chemistry Marks      : %d", Std[i].M_Chem);
+        printf("\n Student Mathematics Marks    : %d", Std[i].M_Maths);
+        printf("\n Student Total Marks          : %d", Std[i].Total);
+        printf("\n Student Percentage           : %d%%", Std[i].Percentage);
+
+        printf("\n\n===================================================");
+
+    }
+
+    getch();
+    return 0;
+}
