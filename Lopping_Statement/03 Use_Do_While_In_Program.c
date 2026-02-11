5e4eeca36de221025c6ee84dc0c02f3b4d10960f
Guruprasad_Jadhav <guruprasadjadhav911@gmail.com>
2026-02-11 11:48:28 +0530

    Use_Do_While_In_Program

---
+++ Lopping_Statement/03 Use_Do_While_In_Program.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i=1;
+
+
+    do
+    {
+        printf("\n %d Guruprasad ",i);
+
+        i++;
+    }
+
+    while(i<=5);
+    {
+
+    };
+
+    getch();
+    return 0;
+}
