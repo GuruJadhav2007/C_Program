1941523fbdf4f8419e1ca1a5f4d3c29384095f26
Guruprasad_Jadhav <guruprasadjadhav911@gmail.com>
2026-02-12 22:25:29 +0530

    B_Table_With_For

---
+++ Lopping_Statement/04 B_Table_With_For.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No = 0, Multi = 1;
+
+    for(No = 2; Multi <= 10 ; Multi++)
+    {
+        printf("\n\n %d",No * Multi);
+    }
+
+    printf("\n\n Thank You");
+
+    getch();
+    return 0;
+}
