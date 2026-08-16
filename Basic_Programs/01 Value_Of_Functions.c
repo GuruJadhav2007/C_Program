f64d71b5baec3cc1ffb83c3d2561891828347e1f
GuruprasadJadhav <guruprasadjadhav911@gmail.com>
2026-08-16 12:35:45 +0530

    Add basic C programs

---
+++ Basic_Programs/01 Value_Of_Functions.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+    char ch ='z';
+    int num = 71;
+    float per = 89.8752;
+
+    printf("\n Welcome to programming");
+
+    printf("\n Value of num => %d $$",num);
+
+    printf("\n Value of character => %c !!",ch);
+
+    printf("\n percentage = %f in STI.",per);
+
+    getch();
+    return 0;
+
+}
