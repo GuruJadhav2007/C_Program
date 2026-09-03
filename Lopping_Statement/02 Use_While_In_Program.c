777e1f7df4481ca999f759841e7bf85fe7bb1fdf
Guruprasad Jadhav <guruprasadjadhav911@gmail.com>
2026-09-03 10:31:43 +0530

    Add files via upload

---
+++ Lopping_Statement/02 Use_While_In_Program.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+
+{
+    int i = 1;
+
+    while(i<=20)
+
+    {
+        printf("\n\n %d Fork Infosystem ",i);
+     i++;
+    }
+
+    getch();
+    return 0;
+}
