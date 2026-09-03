73b178c9ba8afbe74f3fd2a407637567de1652a7
Guruprasad Jadhav <guruprasadjadhav911@gmail.com>
2026-09-03 10:31:19 +0530

    Add files via upload

---
+++ Lopping_Statement/01 Use_For_In_Program.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0;
+
+    printf("\n\t Hello");
+
+    for (i = 1 ;i <= 10; i++)
+    {
+        printf("\n\n %d Karad ",i);
+    }
+
+    printf("\n\n Thank You.");
+
+    getch();
+    return 0;
+}
